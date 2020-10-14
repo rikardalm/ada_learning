d262eb7ada962c1d617db9d4556507880438ea08
Rikard <rikardalm@pop-os.localdomain>
2020-10-14 19:46:25 +0200

    private fix

--- gamla_duggor/dugga_5_pass_1/space_coordinate_handling.ads
+++ gamla_duggor/dugga_5_pass_1/space_coordinate_handling.ads
@@ -1,16 +1,19 @@
 package Space_Coordinate_Handling is
 
-type Space_Coordinate_Type is record
-    X: Float;
-    Y: Float;
-    Z: Float;
-end record;
+
+type Space_Coordinate_Type is private;
 
 function Make_Coordinate(X, Y, Z: in Float) return Space_Coordinate_Type;
 
 procedure Put(Item: in Space_Coordinate_Type; Width: in Integer);
 
 private
+
+type Space_Coordinate_Type is record
+    X: Float;
+    Y: Float;
+    Z: Float;
+end record;
 
 procedure PrintSpace (Width: in Integer);
 
